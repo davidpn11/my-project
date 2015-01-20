f47aba41dfc0838e64027d13033337df74f308e2
David <David@Davids-MacBook-Pro-2.local>
2015-01-20 19:01:17 -0200

    Pegando categorias do servidor A categoria que pega é sentada na mão

---
+++ public/js/angularApp.js
@@ -0,0 +1,111 @@
+(function () {
+    var app = angular.module('news', ['ngRoute']);
+    
+    app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'partials/index',
+        controller: 'ContentController',
+        controllerAs: 'content'
+      })
+        
+     .when('/category', {
+        templateUrl: 'partials/category',
+        controller: 'CategoryController',
+        controllerAs: 'categoriaCtrl'
+      })
+    
+     .when('/contact', {
+        templateUrl: 'partials/contact',
+        
+      })
+    
+     .when('/single-post', {
+        templateUrl: 'partials/single-post',
+        controller: 'ContentController',
+        controllerAs: 'conteudo'
+      })
+     .otherwise({
+        redirectTo: '/'
+      });
+        $locationProvider.html5Mode(true);  
+  }]);
+    
+  
+    
+    
+    
+    
+    app.controller('DateController', function () {
+        this.date = new Date();
+    });
+
+
+    app.controller('ContentController', function (){
+       
+    });
+
+/*
+ console.log($scope.products[0].subtitle);
+            console.log($scope.products[0].image);
+            console.log($scope.products[0].dia);
+            console.log($scope.products[0].mes);
+            console.log($scope.products[0].author);
+            console.log($scope.products[0].dia);
+            console.log($scope.products[0].ano);
+
+*/
+    
+    app.controller('CategoryController',['$scope', '$http', function($scope,$http){
+       
+       $scope.categoria = 'Tecnologia';
+       
+
+        $http.get('/api/posts').success(function(data){            
+            $scope.products = data.posts;
+  //          console.log($scope.products[0].title);
+        });
+       /* console.log('nada');
+        this.postsCategoria = singlePost;
+        this.categoria = "Tecnologia";
+        
+        this.setCat = function(novo){
+            this.categoria = novo;            
+            console.log(this.categoria);
+        };    */
+    
+    }]);
+    
+    
+    
+    
+    
+    
+    app.controller('CommentController',function(){
+        this.comentario = {};
+    
+        this.addComment = function(post){
+            post.comments.push(this.comentario);           
+            this.comentario = {};
+        };
+    });
+    
+    app.controller('viewsController', ['$filter', function ($filter){
+        var orderBy = $filter('orderBy');
+        this.posts = singlePost;
+        this.posts = orderBy(this.posts, '-views', false);
+        
+        
+    }]);
+    
+    app.controller('popularController', ['$filter', function ($filter){
+        var orderBy = $filter('orderBy');
+        this.posts = singlePost;
+        this.posts = orderBy(this.posts, '-comments.length', false);
+        
+    }]);
+
+    
+    
+    
+})();
--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,103 @@
 // initialize our faux database
 var data = {
-  "posts": [
-    {
-      "title": "Lorem ipsum",
-      "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    },
-    {
-      "title": "Sed egestas",
-      "text": "Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus."
-    }
-  ]
+  "posts" : [
+        {
+            "id": 1,
+            "title": "Long Traffic Jams Occur in The City Because there is a Demo Rising Oil Prices",
+            "subtitle": "subtitle",
+            "body": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam el eum iriure dolor in hendrerit in vulputate velit esse molestie consequat; est usus legentis in iis qui facit eorum claritatem. Investigationes lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum. Mirum est notare quam littera.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.",
+            "image":"images/blog/1.jpg",            
+            "imageSpan":"Example : This is image caption fo sample",
+            "category": "Tecnologia",
+            "comments": [{
+                "nome":"Bob Marchetti",
+                "hora":"",
+                "body":"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat"},
+                {
+                "nome":"David Pena",
+                "hora":"",
+                "body":"Colé"}
+            ],
+            "author": "John Smith",
+            "dia":"4",
+            "mes":"Novembro",
+            "ano":"2015",
+            "link": "/post",
+            "views": 0
+        },
+        {
+            "id": 2,
+            "title": "Long Traffic Jams Occur in The City Because there is a Demo Rising Oil Prices",
+            "subtitle": "subtitle",
+            "body": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam el eum iriure dolor in hendrerit in vulputate velit esse molestie consequat; est usus legentis in iis qui facit eorum claritatem. Investigationes lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum. Mirum est notare quam littera.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.",
+            "image":"images/blog/1.jpg",            
+            "imageSpan":"Example : This is image caption fo sample",
+            "category": "Tecnologia",
+            "comments": [{
+                "nome":"Bob Marchetti",
+                "hora":"",
+                "body":"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat"},
+                {
+                "nome":"David Pena",
+                "hora":"",
+                "body":"Colé"}
+            ],
+            "author": "John Smith",
+            "dia":"4",
+            "mes":"Novembro",
+            "ano":"2015",
+            "link": "/post",
+            "views": 0
+        },
+        {
+            "id": 3,
+            "title": "Titulo interessante",
+            "subtitle": "subtitle",
+            "body": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam el eum iriure dolor in hendrerit in vulputate velit esse molestie consequat; est usus legentis in iis qui facit eorum claritatem. Investigationes lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum. Mirum est notare quam littera.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.",
+            "image":"images/blog/1.jpg",            
+            "imageSpan":"Example : This is image caption fo sample",
+            "category": "Esporte",
+            "comments": [{
+                "nome":"Bob Marchetti",
+                "hora":"",
+                "body":"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat"},
+                {
+                "nome":"David Pena",
+                "hora":"",
+                "body":"Colé"}
+            ],
+            "author": "John Smith",
+            "dia":"4",
+            "mes":"Novembro",
+            "ano":"2015",
+            "link": "/post",
+            "views": 0
+        },
+        {
+            "id": 4,
+            "title": "Cruzeiro campeao",
+            "subtitle": "subtitle",
+            "body": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam el eum iriure dolor in hendrerit in vulputate velit esse molestie consequat; est usus legentis in iis qui facit eorum claritatem. Investigationes lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum. Mirum est notare quam littera.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.",
+            "image":"images/blog/1.jpg",            
+            "imageSpan":"Example : This is image caption fo sample",
+            "category": "Mundo",
+            "comments": [{
+                "nome":"Bob Marchetti",
+                "hora":"",
+                "body":"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat"},
+                {
+                "nome":"David Pena",
+                "hora":"",
+                "body":"Colé"}
+            ],
+            "author": "John Smith",
+            "dia":"4",
+            "mes":"Novembro",
+            "ano":"2015",
+            "link": "/post",
+            "views": 0
+        }
+    ]
 };
 
 // GET
@@ -18,9 +106,19 @@ exports.posts = function (req, res) {
   var posts = [];
   data.posts.forEach(function (post, i) {
     posts.push({
-      id: i,
+      id: post.id,
       title: post.title,
-      text: post.text.substr(0, 50) + '...'
+      subtitle: post.subtitle,
+      body: post.body,
+      image: post.image,
+      imageSpan: post.imageSpan,
+      category: post.category,
+      author: post.author,
+      dia: post.dia,
+      mes: post.mes,
+      ano: post.ano,
+      link: post.link,
+      views: post.views
     });
   });
   res.json({
